8bd1bfd19a75df01b67e09fa1debb1a1a2331799
Simon Jahreiß <simon.jahreiss@qaware.de>
2016-08-19 15:03:13 +0200

    Added components for deployment resource.

---
+++ kubernetes/src/main/java/de/qaware/cloud/deployer/kubernetes/resource/deployment/DeploymentClient.java
@@ -0,0 +1,15 @@
+package de.qaware.cloud.deployer.kubernetes.resource.deployment;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+public interface DeploymentClient {
+
+    @GET("/apis/extensions/v1beta1/namespaces/{namespace}/deployments/{name}")
+    Call<ResponseBody> get(@Path("name") String name, @Path("namespace") String namespace);
+
+    @POST("/apis/extensions/v1beta1/namespaces/{namespace}/deployments")
+    Call<ResponseBody> create(@Path("namespace") String namespace, @Body RequestBody deploymentDescription);
+}
---
+++ kubernetes/src/main/java/de/qaware/cloud/deployer/kubernetes/resource/deployment/DeploymentResource.java
@@ -0,0 +1,44 @@
+package de.qaware.cloud.deployer.kubernetes.resource.deployment;
+
+import de.qaware.cloud.deployer.kubernetes.config.resource.ResourceConfig;
+import de.qaware.cloud.deployer.kubernetes.resource.base.BaseResource;
+import de.qaware.cloud.deployer.kubernetes.resource.base.Resource;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+
+public class DeploymentResource extends BaseResource implements Resource {
+
+    private final DeploymentClient deploymentClient;
+
+    public DeploymentResource(String namespace, ResourceConfig resourceConfig) {
+        super(namespace, resourceConfig);
+        this.deploymentClient = createClient(DeploymentClient.class);
+    }
+
+    @Override
+    public boolean exists() {
+        try {
+            Call<ResponseBody> request = deploymentClient.get(getId(), getNamespace());
+            Response<ResponseBody> response = request.execute();
+            return isSuccessResponse(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean create() {
+        try {
+            Call<ResponseBody> request = deploymentClient.create(getNamespace(), createRequestBody());
+            Response<ResponseBody> response = request.execute();
+            return isSuccessResponse(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
