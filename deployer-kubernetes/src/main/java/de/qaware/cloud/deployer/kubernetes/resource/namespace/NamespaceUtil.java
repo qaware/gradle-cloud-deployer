8fbd4c3005096a36122ce70e719d1c24ecab9465
Simon Jahreiß <simon.jahreiss@qaware.de>
2016-09-06 17:34:03 +0200

    Made NamespaceUtil final.

--- deployer-kubernetes/src/main/java/de/qaware/cloud/deployer/kubernetes/resource/namespace/NamespaceUtil.java
+++ deployer-kubernetes/src/main/java/de/qaware/cloud/deployer/kubernetes/resource/namespace/NamespaceUtil.java
@@ -20,7 +20,7 @@ import de.qaware.cloud.deployer.kubernetes.update.HardUpdateStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NamespaceUtil {
+public final class NamespaceUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HardUpdateStrategy.class);
 
