418410674674c32c984b0a2301a7fa2361cbac9a
Simon Jahreiß <simon.jahreiss@qaware.de>
2016-09-08 12:41:03 +0200

    Added comments to the NamespaceResource.

--- deployer-kubernetes/src/main/java/de/qaware/cloud/deployer/kubernetes/resource/namespace/NamespaceResource.java
+++ deployer-kubernetes/src/main/java/de/qaware/cloud/deployer/kubernetes/resource/namespace/NamespaceResource.java
@@ -16,16 +16,28 @@
 package de.qaware.cloud.deployer.kubernetes.resource.namespace;
 
 import de.qaware.cloud.deployer.commons.error.ResourceException;
+import de.qaware.cloud.deployer.commons.resource.ClientFactory;
 import de.qaware.cloud.deployer.kubernetes.config.resource.KubernetesResourceConfig;
 import de.qaware.cloud.deployer.kubernetes.resource.base.KubernetesResource;
-import de.qaware.cloud.deployer.commons.resource.ClientFactory;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 
+/**
+ * Represents a kubernetes namespace which offers methods for deletion and creation.
+ */
 public class NamespaceResource extends KubernetesResource {
 
+    /**
+     * The client which is used for backend communication.
+     */
     private final NamespaceClient namespaceClient;
 
+    /**
+     * Creates a new namespace resource as specified in the config.
+     *
+     * @param resourceConfig The config which describes the namespace.
+     * @param clientFactory The factory which is used to create the client for the backend communication.
+     */
     public NamespaceResource(KubernetesResourceConfig resourceConfig, ClientFactory clientFactory) {
         super(resourceConfig.getResourceId(), resourceConfig, clientFactory);
         this.namespaceClient = createClient(NamespaceClient.class);
