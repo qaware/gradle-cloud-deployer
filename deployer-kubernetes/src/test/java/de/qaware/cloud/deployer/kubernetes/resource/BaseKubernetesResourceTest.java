63812fc91b8b2fcfcf5fed55eeb0533b2402dfd4
Simon Jahreiß <simon.jahreiss@qaware.de>
2016-09-27 15:50:43 +0200

    #12 - Created a BaseKubernetesResourceTest which offers the most necessary functionality required to test kubernetes resources using WireMock.

---
+++ deployer-kubernetes/src/test/java/de/qaware/cloud/deployer/kubernetes/resource/BaseKubernetesResourceTest.java
@@ -0,0 +1,244 @@
+/*
+ * Copyright 2016 QAware GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.qaware.cloud.deployer.kubernetes.resource;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import com.github.tomakehurst.wiremock.matching.UrlPattern;
+import de.qaware.cloud.deployer.commons.config.cloud.AuthConfig;
+import de.qaware.cloud.deployer.commons.config.cloud.EnvironmentConfig;
+import de.qaware.cloud.deployer.commons.config.cloud.SSLConfig;
+import de.qaware.cloud.deployer.commons.config.resource.ContentType;
+import de.qaware.cloud.deployer.commons.error.ResourceConfigException;
+import de.qaware.cloud.deployer.commons.error.ResourceException;
+import de.qaware.cloud.deployer.commons.resource.BaseResource;
+import de.qaware.cloud.deployer.commons.resource.ClientFactory;
+import de.qaware.cloud.deployer.commons.strategy.Strategy;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public abstract class BaseKubernetesResourceTest {
+
+    protected static final String NAMESPACE = "test";
+    private static final String SERVER_ADDRESS = "http://localhost";
+
+    @ClassRule
+    public static WireMockClassRule wireMockRule = new WireMockClassRule(WireMockConfiguration.options().dynamicPort());
+
+    @Rule
+    public WireMockClassRule instanceRule = wireMockRule;
+
+    protected BaseResource resource;
+
+    public abstract BaseResource createResource(ClientFactory clientFactory) throws ResourceException, ResourceConfigException;
+
+    @Before
+    public void setup() throws ResourceException, ResourceConfigException {
+        // Create test environment
+        EnvironmentConfig environmentConfig = new EnvironmentConfig(NAMESPACE, SERVER_ADDRESS + ":" + instanceRule.port(), Strategy.REPLACE);
+        environmentConfig.setAuthConfig(new AuthConfig());
+        environmentConfig.setSslConfig(new SSLConfig());
+        ClientFactory clientFactory = new ClientFactory(environmentConfig);
+
+        // Create resource
+        resource = createResource(clientFactory);
+    }
+
+    @After
+    public void reset() {
+        instanceRule.resetMappings();
+        instanceRule.resetScenarios();
+    }
+
+    protected void testExists(UrlPattern instancePattern) throws ResourceException {
+        String scenarioName = "testExists";
+
+        // Doesn't exist
+        instanceRule.stubFor(get(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(STARTED)
+                .willReturn(aResponse().withStatus(404))
+                .willSetStateTo("existsTrue"));
+
+        // Check exists
+        assertFalse(resource.exists());
+
+        // Exists
+        instanceRule.stubFor(get(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs("existsTrue")
+                .willReturn(aResponse().withStatus(200)));
+
+        // Check exists
+        assertTrue(resource.exists());
+
+        // Verify calls
+        instanceRule.verify(2, getRequestedFor(instancePattern));
+    }
+
+    protected void testCreate(UrlPattern creationPattern, UrlPattern instancePattern) throws ResourceException {
+        String scenarioName = "testCreate";
+
+        // Create
+        instanceRule.stubFor(post(creationPattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(STARTED)
+                .withRequestBody(equalTo(resource.getResourceConfig().getContent()))
+                .willReturn(aResponse().withStatus(201))
+                .willSetStateTo("halfCreated"));
+
+        // Simulate creating
+        instanceRule.stubFor(get(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs("halfCreated")
+                .willReturn(aResponse().withStatus(404))
+                .willSetStateTo("created"));
+
+        // Created
+        instanceRule.stubFor(get(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs("created")
+                .willReturn(aResponse().withStatus(200)));
+
+        // Test
+        resource.create();
+
+        // Verify body
+        instanceRule.verify(postRequestedFor(creationPattern)
+                .withRequestBody(equalTo(resource.getResourceConfig().getContent())));
+
+        // Verify calls
+        instanceRule.verify(1, postRequestedFor(creationPattern));
+        instanceRule.verify(2, getRequestedFor(instancePattern));
+    }
+
+    protected void testCreateRetry(UrlPattern creationPattern, UrlPattern instancePattern) throws ResourceException {
+        String scenarioName = "testCreateRetry";
+
+        // Simulate busy cloud
+        instanceRule.stubFor(post(creationPattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(STARTED)
+                .willReturn(aResponse().withStatus(409))
+                .willSetStateTo("cloudNotBusy"));
+
+        // Create
+        instanceRule.stubFor(post(creationPattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs("cloudNotBusy")
+                .withRequestBody(equalTo(resource.getResourceConfig().getContent()))
+                .willReturn(aResponse().withStatus(201))
+                .willSetStateTo("halfCreated"));
+
+        // Simulate creating
+        instanceRule.stubFor(get(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs("halfCreated")
+                .willReturn(aResponse().withStatus(404))
+                .willSetStateTo("created"));
+
+        // Created
+        instanceRule.stubFor(get(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs("created")
+                .willReturn(aResponse().withStatus(200)));
+
+        // Test
+        resource.create();
+
+        // Verify body
+        instanceRule.verify(postRequestedFor(creationPattern)
+                .withRequestBody(equalTo(resource.getResourceConfig().getContent())));
+
+        // Verify calls
+        instanceRule.verify(2, postRequestedFor(creationPattern));
+        instanceRule.verify(2, getRequestedFor(instancePattern));
+    }
+
+    protected void testDelete(UrlPattern instancePattern) throws ResourceException {
+        String scenarioName = "testDelete";
+
+        // Delete
+        instanceRule.stubFor(delete(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(STARTED)
+                .willReturn(aResponse().withStatus(200))
+                .willSetStateTo("deletionStarted"));
+
+        // Simulate deleting
+        instanceRule.stubFor(get(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs("deletionStarted")
+                .willReturn(aResponse().withStatus(200))
+                .willSetStateTo("deletionEnded"));
+
+        // Deleted
+        instanceRule.stubFor(get(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs("deletionEnded")
+                .willReturn(aResponse().withStatus(404)));
+
+        // Test
+        resource.delete();
+
+        // Verify
+        instanceRule.verify(1, deleteRequestedFor(instancePattern));
+        instanceRule.verify(2, getRequestedFor(instancePattern));
+    }
+
+    protected void testUpdate(UrlPattern instancePattern) throws ResourceException, IOException {
+        String scenarioName = "testUpdate";
+
+        // Prepare body
+        JsonNode body;
+        ContentType contentType = resource.getResourceConfig().getContentType();
+        String content = resource.getResourceConfig().getContent();
+        if (contentType == ContentType.YAML) {
+            body = new ObjectMapper(new YAMLFactory()).readTree(content);
+        } else if (contentType == ContentType.JSON) {
+            body = new ObjectMapper(new JsonFactory()).readTree(content);
+        } else {
+            throw new IllegalArgumentException("Can't process this type of content");
+        }
+        String jsonContent = new ObjectMapper(new JsonFactory()).writeValueAsString(body);
+
+        // Update deployment
+        instanceRule.stubFor(patch(instancePattern)
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(STARTED)
+                .withRequestBody(equalTo(jsonContent))
+                .withHeader("Content-Type", equalTo("application/merge-patch+json; charset=utf-8"))
+                .willReturn(aResponse().withStatus(200)));
+
+        resource.update();
+
+        // Verify calls
+        instanceRule.verify(1, patchRequestedFor(instancePattern));
+    }
+}
--- deployer-kubernetes/src/test/java/de/qaware/cloud/deployer/kubernetes/resource/deployment/DeploymentResourceTest.java
+++ deployer-kubernetes/src/test/java/de/qaware/cloud/deployer/kubernetes/resource/deployment/DeploymentResourceTest.java
@@ -15,175 +15,51 @@
  */
 package de.qaware.cloud.deployer.kubernetes.resource.deployment;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import com.github.tomakehurst.wiremock.matching.UrlPattern;
-import de.qaware.cloud.deployer.commons.config.cloud.AuthConfig;
-import de.qaware.cloud.deployer.commons.config.cloud.EnvironmentConfig;
-import de.qaware.cloud.deployer.commons.config.cloud.SSLConfig;
 import de.qaware.cloud.deployer.commons.config.resource.ContentType;
 import de.qaware.cloud.deployer.commons.config.util.FileUtil;
+import de.qaware.cloud.deployer.commons.error.ResourceConfigException;
 import de.qaware.cloud.deployer.commons.error.ResourceException;
+import de.qaware.cloud.deployer.commons.resource.BaseResource;
 import de.qaware.cloud.deployer.commons.resource.ClientFactory;
-import de.qaware.cloud.deployer.commons.strategy.Strategy;
 import de.qaware.cloud.deployer.kubernetes.config.resource.KubernetesResourceConfig;
-import org.junit.*;
+import de.qaware.cloud.deployer.kubernetes.resource.BaseKubernetesResourceTest;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-public class DeploymentResourceTest {
+public class DeploymentResourceTest extends BaseKubernetesResourceTest {
 
-    private static final String SERVER_ADDRESS = "http://localhost";
-    private static final String NAMESPACE = "test-deployment";
-    private static final UrlPattern DEPLOYMENTS_PATTERN = urlEqualTo("/apis/extensions/v1beta1/namespaces/test/deployments");
-    private static final UrlPattern DEPLOYMENT_PATTERN = urlEqualTo("/apis/extensions/v1beta1/namespaces/test/deployments/zwitscher-eureka");
-    private static final UrlPattern SCALE_PATTERN = urlEqualTo("/apis/extensions/v1beta1/namespaces/test/deployments/zwitscher-eureka/scale");
-    private static final UrlPattern REPLICA_SETS_PATTERN = urlEqualTo("/apis/extensions/v1beta1/namespaces/test/replicasets?labelSelector=deployment-id%3Dzwitscher-eureka");
+    private static final String BASE_PATH = "/apis/extensions/v1beta1/namespaces/" + NAMESPACE;
+    private static final UrlPattern DEPLOYMENTS_PATTERN = urlEqualTo(BASE_PATH + "/deployments");
+    private static final UrlPattern DEPLOYMENT_PATTERN = urlEqualTo(BASE_PATH + "/deployments/zwitscher-eureka");
+    private static final UrlPattern SCALE_PATTERN = urlEqualTo(BASE_PATH + "/deployments/zwitscher-eureka/scale");
+    private static final UrlPattern REPLICA_SETS_PATTERN = urlEqualTo(BASE_PATH + "/replicasets?labelSelector=deployment-id%3Dzwitscher-eureka");
 
-    @ClassRule
-    public static WireMockClassRule wireMockRule = new WireMockClassRule(WireMockConfiguration.options().dynamicPort());
-
-    @Rule
-    public WireMockClassRule instanceRule = wireMockRule;
-
-    private DeploymentResource deploymentResource;
-
-    @Before
-    public void setUp() throws Exception {
-        // Create test environment
-        EnvironmentConfig environmentConfig = new EnvironmentConfig(NAMESPACE, SERVER_ADDRESS + ":" + instanceRule.port(), Strategy.REPLACE);
-        environmentConfig.setAuthConfig(new AuthConfig());
-        environmentConfig.setSslConfig(new SSLConfig());
-        ClientFactory clientFactory = new ClientFactory(environmentConfig);
-
-        // Create the deployment resource
+    @Override
+    public BaseResource createResource(ClientFactory clientFactory) throws ResourceException, ResourceConfigException {
         String deploymentDescriptionV1 = FileUtil.readFileContent("/de/qaware/cloud/deployer/kubernetes/resource/deployment/deployment.yml");
         KubernetesResourceConfig resourceConfigV1 = new KubernetesResourceConfig("test", ContentType.YAML, deploymentDescriptionV1);
-        deploymentResource = new DeploymentResource("test", resourceConfigV1, clientFactory);
-    }
-
-    @After
-    public void reset() {
-        instanceRule.resetMappings();
-        instanceRule.resetScenarios();
+        return new DeploymentResource("test", resourceConfigV1, clientFactory);
     }
 
     @Test
     public void testExists() throws ResourceException {
-        String scenarioName = "testExists";
-
-        // Doesn't exist
-        instanceRule.stubFor(get(DEPLOYMENT_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs(STARTED)
-                .willReturn(aResponse().withStatus(404))
-                .willSetStateTo("existsTrue"));
-
-        // Check exists
-        assertFalse(deploymentResource.exists());
-
-        // Exists
-        instanceRule.stubFor(get(DEPLOYMENT_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs("existsTrue")
-                .willReturn(aResponse().withStatus(200)));
-
-        // Check exists
-        assertTrue(deploymentResource.exists());
-
-        // Verify calls
-        instanceRule.verify(2, getRequestedFor(DEPLOYMENT_PATTERN));
+        testExists(DEPLOYMENT_PATTERN);
     }
 
     @Test
     public void testCreate() throws ResourceException {
-        String scenarioName = "testCreate";
-
-        // Create
-        instanceRule.stubFor(post(DEPLOYMENTS_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs(STARTED)
-                .withRequestBody(equalTo(deploymentResource.getResourceConfig().getContent()))
-                .willReturn(aResponse().withStatus(201))
-                .willSetStateTo("deploymentHalfCreated"));
-
-        // Simulate creating
-        instanceRule.stubFor(get(DEPLOYMENT_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs("deploymentHalfCreated")
-                .willReturn(aResponse().withStatus(404))
-                .willSetStateTo("deploymentCreated"));
-
-        // Created
-        instanceRule.stubFor(get(DEPLOYMENT_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs("deploymentCreated")
-                .willReturn(aResponse().withStatus(200)));
-
-        // Create deployment
-        deploymentResource.create();
-
-        // Verify body
-        instanceRule.verify(postRequestedFor(DEPLOYMENTS_PATTERN)
-                .withRequestBody(equalTo(deploymentResource.getResourceConfig().getContent())));
-
-        // Verify calls
-        instanceRule.verify(1, postRequestedFor(DEPLOYMENTS_PATTERN));
-        instanceRule.verify(2, getRequestedFor(DEPLOYMENT_PATTERN));
+        testCreate(DEPLOYMENTS_PATTERN, DEPLOYMENT_PATTERN);
     }
 
     @Test
     public void testCreateRetry() throws ResourceException {
-        String scenarioName = "testCreateRetry";
-
-        // Simulate busy cloud
-        instanceRule.stubFor(post(DEPLOYMENTS_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs(STARTED)
-                .willReturn(aResponse().withStatus(409))
-                .willSetStateTo("cloudNotBusy"));
-
-        // Create
-        instanceRule.stubFor(post(DEPLOYMENTS_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs("cloudNotBusy")
-                .withRequestBody(equalTo(deploymentResource.getResourceConfig().getContent()))
-                .willReturn(aResponse().withStatus(201))
-                .willSetStateTo("deploymentHalfCreated"));
-
-        // Simulate creating
-        instanceRule.stubFor(get(DEPLOYMENT_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs("deploymentHalfCreated")
-                .willReturn(aResponse().withStatus(404))
-                .willSetStateTo("deploymentCreated"));
-
-        // Created
-        instanceRule.stubFor(get(DEPLOYMENT_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs("deploymentCreated")
-                .willReturn(aResponse().withStatus(200)));
-
-        // Create deployment
-        deploymentResource.create();
-
-        // Verify body
-        instanceRule.verify(postRequestedFor(DEPLOYMENTS_PATTERN)
-                .withRequestBody(equalTo(deploymentResource.getResourceConfig().getContent())));
-
-        // Verify calls
-        instanceRule.verify(2, postRequestedFor(DEPLOYMENTS_PATTERN));
-        instanceRule.verify(2, getRequestedFor(DEPLOYMENT_PATTERN));
+        testCreateRetry(DEPLOYMENTS_PATTERN, DEPLOYMENT_PATTERN);
     }
 
     @Test
@@ -231,7 +107,7 @@ public class DeploymentResourceTest {
                 .whenScenarioStateIs("replicaSetDeleted")
                 .willReturn(aResponse().withStatus(404)));
 
-        deploymentResource.delete();
+        resource.delete();
 
         // Verify calls
         instanceRule.verify(1, putRequestedFor(SCALE_PATTERN));
@@ -242,22 +118,6 @@ public class DeploymentResourceTest {
 
     @Test
     public void testUpdate() throws ResourceException, TimeoutException, InterruptedException, IOException {
-        String scenarioName = "testUpdate";
-        JsonNode yamlBody = new ObjectMapper(new YAMLFactory()).readTree(deploymentResource.getResourceConfig().getContent());
-        String jsonBody = new ObjectMapper(new JsonFactory()).writeValueAsString(yamlBody);
-
-        // Update deployment
-        instanceRule.stubFor(patch(DEPLOYMENT_PATTERN)
-                .inScenario(scenarioName)
-                .whenScenarioStateIs(STARTED)
-                .withRequestBody(equalTo(jsonBody))
-                .withHeader("Content-Type", equalTo("application/merge-patch+json; charset=utf-8"))
-                .willReturn(aResponse().withStatus(200))
-                .willSetStateTo("scaledDown"));
-
-        deploymentResource.update();
-
-        // Verify calls
-        instanceRule.verify(1, patchRequestedFor(DEPLOYMENT_PATTERN));
+        testUpdate(DEPLOYMENT_PATTERN);
     }
 }
