18761af8db381899e3a1e7fa7b3553f9c3d7726a
Simon Jahreiß <simon.jahreiss@qaware.de>
2016-09-27 18:03:19 +0200

    #12 - Refactored KubernetesResourceFactoryTest using WireMock.

--- deployer-kubernetes/src/test/java/de/qaware/cloud/deployer/kubernetes/resource/KubernetesResourceFactoryTest.java
+++ deployer-kubernetes/src/test/java/de/qaware/cloud/deployer/kubernetes/resource/KubernetesResourceFactoryTest.java
@@ -15,43 +15,74 @@
  */
 package de.qaware.cloud.deployer.kubernetes.resource;
 
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import de.qaware.cloud.deployer.commons.config.resource.ContentType;
 import de.qaware.cloud.deployer.commons.config.util.FileUtil;
 import de.qaware.cloud.deployer.commons.error.ResourceConfigException;
 import de.qaware.cloud.deployer.commons.error.ResourceException;
+import de.qaware.cloud.deployer.commons.strategy.Strategy;
 import de.qaware.cloud.deployer.kubernetes.config.cloud.KubernetesEnvironmentConfig;
+import de.qaware.cloud.deployer.kubernetes.config.namespace.NamespaceResourceConfigFactory;
 import de.qaware.cloud.deployer.kubernetes.config.resource.KubernetesResourceConfig;
 import de.qaware.cloud.deployer.kubernetes.resource.base.KubernetesResource;
 import de.qaware.cloud.deployer.kubernetes.resource.deployment.DeploymentResource;
-import de.qaware.cloud.deployer.kubernetes.resource.namespace.NamespaceResource;
 import de.qaware.cloud.deployer.kubernetes.resource.pod.PodResource;
 import de.qaware.cloud.deployer.kubernetes.resource.replication.controller.ReplicationControllerResource;
 import de.qaware.cloud.deployer.kubernetes.resource.service.ServiceResource;
-import de.qaware.cloud.deployer.kubernetes.test.KubernetesTestEnvironment;
-import de.qaware.cloud.deployer.kubernetes.test.KubernetesTestEnvironmentUtil;
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static de.qaware.cloud.deployer.commons.logging.CommonsMessageBundle.COMMONS_MESSAGE_BUNDLE;
 import static de.qaware.cloud.deployer.kubernetes.logging.KubernetesMessageBundle.KUBERNETES_MESSAGE_BUNDLE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-public class KubernetesResourceFactoryTest extends TestCase {
+public class KubernetesResourceFactoryTest {
 
+    private static final String NAMESPACE = "test-namespace";
     private static final String TEMP_FILE = "temp";
 
-    private KubernetesResourceConfig resourceConfig;
-    private KubernetesResourceFactory resourceFactory;
-    private NamespaceResource namespaceResource;
+    @ClassRule
+    public static WireMockClassRule wireMockRule = new WireMockClassRule(WireMockConfiguration.options().dynamicPort());
 
-    @Override
+    @Rule
+    public WireMockClassRule instanceRule = wireMockRule;
+
+    private KubernetesResourceFactory resourceFactory;
+
+    @Before
     public void setUp() throws Exception {
-        KubernetesTestEnvironment testEnvironment = KubernetesTestEnvironmentUtil.createTestEnvironment();
-        KubernetesEnvironmentConfig environmentConfig = testEnvironment.getEnvironmentConfig();
-        resourceConfig = testEnvironment.getNamespaceResource().getResourceConfig();
-        namespaceResource = testEnvironment.getNamespaceResource();
+        instanceRule.stubFor(get(urlEqualTo("/api/v1/namespaces"))
+                .willReturn(aResponse().withStatus(200)));
+
+        KubernetesEnvironmentConfig environmentConfig = new KubernetesEnvironmentConfig("test", "http://localhost:" + instanceRule.port(), Strategy.REPLACE, NAMESPACE);
         resourceFactory = new KubernetesResourceFactory(environmentConfig);
     }
 
+    @Test
+    public void testFactoryCreationWithFailingPing() throws ResourceConfigException {
+        boolean exceptionThrown = false;
+        instanceRule.resetMappings();
+        instanceRule.stubFor(get(urlEqualTo("/api/v1/namespaces"))
+                .willReturn(aResponse().withStatus(404)));
+        KubernetesEnvironmentConfig environmentConfig = new KubernetesEnvironmentConfig("test", "http://localhost:" + instanceRule.port(), Strategy.REPLACE, NAMESPACE);
+        try {
+            resourceFactory = new KubernetesResourceFactory(environmentConfig);
+        } catch (ResourceException e) {
+            exceptionThrown = true;
+            assertEquals(COMMONS_MESSAGE_BUNDLE.getMessage("DEPLOYER_COMMONS_ERROR_PING_FAILED", "test", 404), e.getMessage());
+        }
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
     public void testCreateWithEmptyResourceConfig() throws ResourceException, ResourceConfigException {
         boolean exceptionThrown = false;
+        KubernetesResourceConfig resourceConfig = NamespaceResourceConfigFactory.create(NAMESPACE);
         resourceConfig.setContent("");
         try {
             resourceFactory.createResource(resourceConfig);
@@ -62,6 +93,7 @@ public class KubernetesResourceFactoryTest extends TestCase {
         assertTrue(exceptionThrown);
     }
 
+    @Test
     public void testCreateWithValidVersionButUnknownType() throws ResourceConfigException {
         // Version: extensions/v1beta1
         String content = "apiVersion: extensions/v1beta1\n" +
@@ -80,6 +112,7 @@ public class KubernetesResourceFactoryTest extends TestCase {
         assertException(content, message);
     }
 
+    @Test
     public void testCreateWithValidTypeButUnknownVersion() throws ResourceConfigException {
         String content = "apiVersion: unknown\n" +
                 "kind: Deployment\n" +
@@ -89,36 +122,40 @@ public class KubernetesResourceFactoryTest extends TestCase {
         assertException(content, message);
     }
 
+    @Test
     public void testCreateWithValidDeployment() throws ResourceConfigException, ResourceException {
-        KubernetesResourceConfig config = new KubernetesResourceConfig("temp", ContentType.YAML, FileUtil.readFileContent("/deployment/deployment.yml"));
+        KubernetesResourceConfig config = new KubernetesResourceConfig("temp", ContentType.YAML, FileUtil.readFileContent(getTestFilePath("deployment.yml")));
         KubernetesResource deploymentResource = resourceFactory.createResource(config);
         assertTrue(deploymentResource instanceof DeploymentResource);
         assertEquals("zwitscher-eureka", deploymentResource.getId());
-        assertEquals(namespaceResource.getId(), deploymentResource.getNamespace());
+        assertEquals(NAMESPACE, deploymentResource.getNamespace());
     }
 
+    @Test
     public void testCreateWithValidPod() throws ResourceConfigException, ResourceException {
-        KubernetesResourceConfig config = new KubernetesResourceConfig("temp", ContentType.JSON, FileUtil.readFileContent("/pod/pod.json"));
+        KubernetesResourceConfig config = new KubernetesResourceConfig("temp", ContentType.JSON, FileUtil.readFileContent(getTestFilePath("pod.json")));
         KubernetesResource podResource = resourceFactory.createResource(config);
         assertTrue(podResource instanceof PodResource);
         assertEquals("nginx-mysql", podResource.getId());
-        assertEquals(namespaceResource.getId(), podResource.getNamespace());
+        assertEquals(NAMESPACE, podResource.getNamespace());
     }
 
+    @Test
     public void testCreateWithValidService() throws ResourceConfigException, ResourceException {
-        KubernetesResourceConfig config = new KubernetesResourceConfig("temp", ContentType.YAML, FileUtil.readFileContent("/service/service.yml"));
+        KubernetesResourceConfig config = new KubernetesResourceConfig("temp", ContentType.YAML, FileUtil.readFileContent(getTestFilePath("service.yml")));
         KubernetesResource serviceResource = resourceFactory.createResource(config);
         assertTrue(serviceResource instanceof ServiceResource);
         assertEquals("zwitscher-eureka", serviceResource.getId());
-        assertEquals(namespaceResource.getId(), serviceResource.getNamespace());
+        assertEquals(NAMESPACE, serviceResource.getNamespace());
     }
 
+    @Test
     public void testCreateWithValidReplicationController() throws ResourceConfigException, ResourceException {
-        KubernetesResourceConfig config = new KubernetesResourceConfig("temp", ContentType.YAML, FileUtil.readFileContent("/replication/controller/replication-controller.yml"));
+        KubernetesResourceConfig config = new KubernetesResourceConfig("temp", ContentType.YAML, FileUtil.readFileContent(getTestFilePath("replication-controller.yml")));
         KubernetesResource controllerResource = resourceFactory.createResource(config);
         assertTrue(controllerResource instanceof ReplicationControllerResource);
         assertEquals("nginx", controllerResource.getId());
-        assertEquals(namespaceResource.getId(), controllerResource.getNamespace());
+        assertEquals(NAMESPACE, controllerResource.getNamespace());
     }
 
     private void assertException(String content, String exceptionMessage) throws ResourceConfigException {
@@ -131,5 +168,9 @@ public class KubernetesResourceFactoryTest extends TestCase {
             assertEquals(exceptionMessage, e.getMessage());
         }
         assertTrue(exceptionThrown);
+    }
+
+    private String getTestFilePath(String filename) {
+        return "/de/qaware/cloud/deployer/kubernetes/resource/factory/" + filename;
     }
 }
