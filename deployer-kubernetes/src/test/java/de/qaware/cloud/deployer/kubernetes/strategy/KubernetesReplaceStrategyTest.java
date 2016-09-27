08379981da0c80febd3abedc4a4aaf20bad90b80
Simon Jahreiß <simon.jahreiss@qaware.de>
2016-09-27 19:56:42 +0200

    #12 - Implemented KubernetesReplaceStrategyTest using Mockito.

--- deployer-kubernetes/src/test/java/de/qaware/cloud/deployer/kubernetes/strategy/KubernetesReplaceStrategyTest.java
+++ deployer-kubernetes/src/test/java/de/qaware/cloud/deployer/kubernetes/strategy/KubernetesReplaceStrategyTest.java
@@ -16,197 +16,152 @@
 package de.qaware.cloud.deployer.kubernetes.strategy;
 
 import de.qaware.cloud.deployer.commons.error.ResourceException;
-import de.qaware.cloud.deployer.commons.strategy.Strategy;
-import de.qaware.cloud.deployer.kubernetes.config.cloud.KubernetesEnvironmentConfig;
-import de.qaware.cloud.deployer.kubernetes.config.resource.KubernetesResourceConfig;
-import de.qaware.cloud.deployer.kubernetes.config.resource.KubernetesResourceConfigFactory;
-import de.qaware.cloud.deployer.kubernetes.resource.KubernetesResourceFactory;
-import de.qaware.cloud.deployer.kubernetes.resource.base.KubernetesResource;
-import de.qaware.cloud.deployer.kubernetes.resource.namespace.NamespaceResource;
-import de.qaware.cloud.deployer.kubernetes.test.*;
-import io.fabric8.kubernetes.api.model.Pod;
-import io.fabric8.kubernetes.api.model.PodList;
-import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.api.model.extensions.Deployment;
-import io.fabric8.kubernetes.api.model.extensions.ReplicaSet;
-import io.fabric8.kubernetes.api.model.extensions.ReplicaSetList;
-import io.fabric8.kubernetes.client.KubernetesClient;
-import junit.framework.TestCase;
+import de.qaware.cloud.deployer.kubernetes.test.BaseKubernetesStrategyTest;
+import org.junit.Test;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeoutException;
-import java.util.stream.Collectors;
+import static org.mockito.Mockito.*;
 
-public class KubernetesReplaceStrategyTest extends TestCase {
+public class KubernetesReplaceStrategyTest extends BaseKubernetesStrategyTest {
 
-    private NamespaceResource namespaceResource;
-    private KubernetesReplaceStrategy replaceStrategy;
-    private List<KubernetesResource> resourcesV1;
-    private List<KubernetesResource> resourcesV2;
-    private KubernetesClient kubernetesClient;
+    @Test
+    public void testDeployWithNotExistingNamespace() throws ResourceException {
+        // Reset
+        KubernetesReplaceStrategy strategy = new KubernetesReplaceStrategy();
+        strategy.deploy(namespaceResource, resources);
 
-    @Override
-    public void setUp() throws Exception {
-        // Create test environment
-        KubernetesTestEnvironment testEnvironment = KubernetesTestEnvironmentUtil.createTestEnvironment(Strategy.REPLACE);
-        namespaceResource = testEnvironment.getNamespaceResource();
-        kubernetesClient = testEnvironment.getKubernetesClient();
-        KubernetesEnvironmentConfig environmentConfig = testEnvironment.getEnvironmentConfig();
-        KubernetesTestEnvironmentUtil.createTestNamespace(namespaceResource);
+        // Verify
+        // Create the namespace
+        verify(namespaceResource, times(1)).create();
+        verify(namespaceResource, times(0)).update();
+        verify(namespaceResource, times(1)).exists();
+        verify(namespaceResource, times(0)).delete();
 
-        // Create strategy
-        replaceStrategy = new KubernetesReplaceStrategy();
+        // Create the deployment
+        verify(deploymentResource, times(1)).create();
+        verify(deploymentResource, times(0)).update();
+        verify(deploymentResource, times(1)).exists();
+        verify(deploymentResource, times(0)).delete();
 
-        // Create config and resource factory
-        KubernetesResourceConfigFactory resourceConfigFactory = new KubernetesResourceConfigFactory();
-        KubernetesResourceFactory factory = new KubernetesResourceFactory(environmentConfig);
-
-        // Create the resources for v1
-        List<File> filesV1 = new ArrayList<>();
-        filesV1.add(new File(this.getClass().getResource("/strategy/replace-strategy-v1.yml").getPath()));
-        List<KubernetesResourceConfig> configsV1 = resourceConfigFactory.createConfigs(filesV1);
-        resourcesV1 = factory.createResources(configsV1);
-
-        // Create the resources for v2
-        List<File> filesV2 = new ArrayList<>();
-        filesV2.add(new File(this.getClass().getResource("/strategy/replace-strategy-v2.yml").getPath()));
-        List<KubernetesResourceConfig> configsV2 = resourceConfigFactory.createConfigs(filesV2);
-        resourcesV2 = factory.createResources(configsV2);
+        // Create the service
+        verify(serviceResource, times(1)).create();
+        verify(serviceResource, times(0)).update();
+        verify(serviceResource, times(1)).exists();
+        verify(serviceResource, times(0)).delete();
     }
 
-    @Override
-    public void tearDown() throws Exception {
-        namespaceResource.delete();
+    @Test
+    public void testDeployWithExistingNamespace() throws ResourceException {
+        // Tune resources - namespace exists already
+        when(namespaceResource.exists()).thenReturn(true);
+
+        // Reset
+        KubernetesReplaceStrategy strategy = new KubernetesReplaceStrategy();
+        strategy.deploy(namespaceResource, resources);
+
+        // Verify
+        // Ignore the namespace
+        verify(namespaceResource, times(0)).create();
+        verify(namespaceResource, times(0)).update();
+        verify(namespaceResource, times(1)).exists();
+        verify(namespaceResource, times(0)).delete();
+
+        // Create the deployment
+        verify(deploymentResource, times(1)).create();
+        verify(deploymentResource, times(0)).update();
+        verify(deploymentResource, times(1)).exists();
+        verify(deploymentResource, times(0)).delete();
+
+        // Create the service
+        verify(serviceResource, times(1)).create();
+        verify(serviceResource, times(0)).update();
+        verify(serviceResource, times(1)).exists();
+        verify(serviceResource, times(0)).delete();
     }
 
-    public void testSingleDeployment() throws ResourceException {
-        // Deploy v1
-        replaceStrategy.deploy(namespaceResource, resourcesV1);
-        String version = "v1";
+    @Test
+    public void testDeployWithExistingResources() throws ResourceException {
+        // Tune resources
+        when(namespaceResource.exists()).thenReturn(true);
+        when(deploymentResource.exists()).thenReturn(true);
 
-        // Check that everything was deployed correctly
-        KubernetesResource serviceResource1 = resourcesV1.get(0);
-        KubernetesResource deploymentResource1 = resourcesV1.get(1);
-        KubernetesResource serviceResource2 = resourcesV1.get(2);
-        KubernetesResource deploymentResource2 = resourcesV1.get(3);
+        // Reset
+        KubernetesReplaceStrategy strategy = new KubernetesReplaceStrategy();
+        strategy.deploy(namespaceResource, resources);
 
-        // Check services
-        assertEquals(2, KubernetesClientUtil.retrieveServices(kubernetesClient, namespaceResource).getItems().size());
-        Service service1 = KubernetesClientUtil.retrieveService(kubernetesClient, serviceResource1);
-        Service service2 = KubernetesClientUtil.retrieveService(kubernetesClient, serviceResource2);
-        CheckUtil.checkService(serviceResource1, service1, version, 8761);
-        CheckUtil.checkService(serviceResource2, service2, version, 8888);
+        // Verify
+        // Ignore the namespace
+        verify(namespaceResource, times(0)).create();
+        verify(namespaceResource, times(0)).update();
+        verify(namespaceResource, times(1)).exists();
+        verify(namespaceResource, times(0)).delete();
 
-        // Check deployments
-        assertEquals(2, KubernetesClientUtil.retrieveDeployments(kubernetesClient, namespaceResource).getItems().size());
-        Deployment deployment1 = KubernetesClientUtil.retrieveDeployment(kubernetesClient, deploymentResource1);
-        Deployment deployment2 = KubernetesClientUtil.retrieveDeployment(kubernetesClient, deploymentResource2);
-        CheckUtil.checkDeployment(deploymentResource1, deployment1, version);
-        CheckUtil.checkDeployment(deploymentResource2, deployment2, version);
+        // Replace the deployment
+        verify(deploymentResource, times(1)).create();
+        verify(deploymentResource, times(0)).update();
+        verify(deploymentResource, times(1)).exists();
+        verify(deploymentResource, times(1)).delete();
 
-        // Check pods
-        PodList podList = KubernetesClientUtil.retrievePods(kubernetesClient, namespaceResource);
-        assertEquals(3, podList.getItems().size());
-
-        List<Pod> pods1 = podList.getItems().stream().filter(pod -> pod.getMetadata().getName().contains(deploymentResource1.getId())).collect(Collectors.toList());
-        assertEquals(2, pods1.size());
-        Pod pod1a = pods1.get(0);
-        Pod pod1b = pods1.get(1);
-        CheckUtil.checkPod(deploymentResource1, pod1a, version);
-        CheckUtil.checkPod(deploymentResource1, pod1b, version);
-
-        List<Pod> pods2 = podList.getItems().stream().filter(pod -> pod.getMetadata().getName().contains(deploymentResource2.getId())).collect(Collectors.toList());
-        assertEquals(1, pods2.size());
-        Pod pod2 = pods2.get(0);
-        CheckUtil.checkPod(deploymentResource2, pod2, version);
-
-        // Check replica sets
-        ReplicaSetList replicaSetList = KubernetesClientUtil.retrieveReplicaSets(kubernetesClient, namespaceResource);
-        assertEquals(2, replicaSetList.getItems().size());
-
-        List<ReplicaSet> replicaSets1 = replicaSetList.getItems().stream().filter(replicaSet -> replicaSet.getMetadata().getName().contains(deploymentResource1.getId())).collect(Collectors.toList());
-        assertEquals(1, replicaSets1.size());
-        ReplicaSet replicaSet1 = replicaSets1.get(0);
-        CheckUtil.checkReplicaSet(deploymentResource1, replicaSet1, version);
-
-        List<ReplicaSet> replicaSets2 = replicaSetList.getItems().stream().filter(replicaSet -> replicaSet.getMetadata().getName().contains(deploymentResource2.getId())).collect(Collectors.toList());
-        assertEquals(1, replicaSets2.size());
-        ReplicaSet replicaSet2 = replicaSets2.get(0);
-        CheckUtil.checkReplicaSet(deploymentResource2, replicaSet2, version);
+        // Create the service
+        verify(serviceResource, times(1)).create();
+        verify(serviceResource, times(0)).update();
+        verify(serviceResource, times(1)).exists();
+        verify(serviceResource, times(0)).delete();
     }
 
-    public void testMultipleDeployments() throws ResourceException, TimeoutException, InterruptedException {
-        // Deploy v1 - already tested above
-        replaceStrategy.deploy(namespaceResource, resourcesV1);
-        String version1 = "v1";
+    @Test
+    public void testDeleteWithNotExistingNamespace() throws ResourceException {
+        // Delete
+        KubernetesReplaceStrategy strategy = new KubernetesReplaceStrategy();
+        strategy.delete(namespaceResource, resources);
 
-        KubernetesResource deploymentResource0 = resourcesV1.get(1);
-        List<Pod> pods0 = KubernetesClientUtil.retrievePods(kubernetesClient, deploymentResource0).getItems();
-        assertEquals(3, pods0.size());
-        Pod pod0a = pods0.get(0);
-        Pod pod0b = pods0.get(1);
-        Pod pod0c = pods0.get(1);
-        PodDeletionBlocker podDeletionBlocker0a = new PodDeletionBlocker(kubernetesClient, pod0a);
-        PodDeletionBlocker podDeletionBlocker0b = new PodDeletionBlocker(kubernetesClient, pod0b);
-        PodDeletionBlocker podDeletionBlocker0c = new PodDeletionBlocker(kubernetesClient, pod0c);
+        // Verify
+        // Ignore the namespace
+        verify(namespaceResource, times(0)).create();
+        verify(namespaceResource, times(0)).update();
+        verify(namespaceResource, times(0)).exists();
+        verify(namespaceResource, times(0)).delete();
 
-        // Deploy v2
-        replaceStrategy.deploy(namespaceResource, resourcesV2);
-        String version2 = "v2";
+        // Ignore the deployment
+        verify(deploymentResource, times(0)).create();
+        verify(deploymentResource, times(0)).update();
+        verify(deploymentResource, times(1)).exists();
+        verify(deploymentResource, times(0)).delete();
 
-        // Wait until the pods are deleted
-        podDeletionBlocker0a.block();
-        podDeletionBlocker0b.block();
-        podDeletionBlocker0c.block();
+        // Ignore the service
+        verify(serviceResource, times(0)).create();
+        verify(serviceResource, times(0)).update();
+        verify(serviceResource, times(1)).exists();
+        verify(serviceResource, times(0)).delete();
+    }
 
-        // Check that everything was deployed correctly
-        KubernetesResource serviceResource1 = resourcesV1.get(2);
-        KubernetesResource deploymentResource1 = resourcesV1.get(3);
-        KubernetesResource serviceResource2 = resourcesV2.get(0);
-        KubernetesResource deploymentResource2 = resourcesV2.get(1);
+    @Test
+    public void testDeleteWithExistingResources() throws ResourceException {
+        // Tune resources
+        when(namespaceResource.exists()).thenReturn(true);
+        when(deploymentResource.exists()).thenReturn(true);
+        when(serviceResource.exists()).thenReturn(true);
 
-        // Check services
-        assertEquals(2, KubernetesClientUtil.retrieveServices(kubernetesClient, namespaceResource).getItems().size());
-        Service service1 = KubernetesClientUtil.retrieveService(kubernetesClient, serviceResource1);
-        Service service2 = KubernetesClientUtil.retrieveService(kubernetesClient, serviceResource2);
-        CheckUtil.checkService(serviceResource1, service1, version1, 8888);
-        CheckUtil.checkService(serviceResource2, service2, version2, 8761);
+        // Delete
+        KubernetesReplaceStrategy strategy = new KubernetesReplaceStrategy();
+        strategy.delete(namespaceResource, resources);
 
-        // Check deployments
-        assertEquals(2, KubernetesClientUtil.retrieveDeployments(kubernetesClient, namespaceResource).getItems().size());
-        Deployment deployment1 = KubernetesClientUtil.retrieveDeployment(kubernetesClient, deploymentResource1);
-        Deployment deployment2 = KubernetesClientUtil.retrieveDeployment(kubernetesClient, deploymentResource2);
-        CheckUtil.checkDeployment(deploymentResource1, deployment1, version1);
-        CheckUtil.checkDeployment(deploymentResource2, deployment2, version2);
+        // Verify
+        // Ignore the namespace
+        verify(namespaceResource, times(0)).create();
+        verify(namespaceResource, times(0)).update();
+        verify(namespaceResource, times(0)).exists();
+        verify(namespaceResource, times(0)).delete();
 
-        // Check pods
-        PodList podList = KubernetesClientUtil.retrievePods(kubernetesClient, namespaceResource);
-        assertEquals(2, podList.getItems().size());
+        // Delete the deployment
+        verify(deploymentResource, times(0)).create();
+        verify(deploymentResource, times(0)).update();
+        verify(deploymentResource, times(1)).exists();
+        verify(deploymentResource, times(1)).delete();
 
-        List<Pod> pods1 = podList.getItems().stream().filter(pod -> pod.getMetadata().getName().contains(deploymentResource1.getId())).collect(Collectors.toList());
-        assertEquals(1, pods1.size());
-        Pod pod1 = pods1.get(0);
-        CheckUtil.checkPod(deploymentResource1, pod1, version1);
-
-        List<Pod> pods2 = podList.getItems().stream().filter(pod -> pod.getMetadata().getName().contains(deploymentResource2.getId())).collect(Collectors.toList());
-        assertEquals(1, pods2.size());
-        Pod pod2 = pods2.get(0);
-        CheckUtil.checkPod(deploymentResource2, pod2, version2);
-
-        // Check replica sets
-        ReplicaSetList replicaSetList = KubernetesClientUtil.retrieveReplicaSets(kubernetesClient, namespaceResource);
-        assertEquals(2, replicaSetList.getItems().size());
-
-        List<ReplicaSet> replicaSets1 = replicaSetList.getItems().stream().filter(replicaSet -> replicaSet.getMetadata().getName().contains(deploymentResource1.getId())).collect(Collectors.toList());
-        assertEquals(1, replicaSets1.size());
-        ReplicaSet replicaSet1 = replicaSets1.get(0);
-        CheckUtil.checkReplicaSet(deploymentResource1, replicaSet1, version1);
-
-        List<ReplicaSet> replicaSets2 = replicaSetList.getItems().stream().filter(replicaSet -> replicaSet.getMetadata().getName().contains(deploymentResource2.getId())).collect(Collectors.toList());
-        assertEquals(1, replicaSets2.size());
-        ReplicaSet replicaSet2 = replicaSets2.get(0);
-        CheckUtil.checkReplicaSet(deploymentResource2, replicaSet2, version2);
+        // Delete the service
+        verify(serviceResource, times(0)).create();
+        verify(serviceResource, times(0)).update();
+        verify(serviceResource, times(1)).exists();
+        verify(serviceResource, times(1)).delete();
     }
 }
