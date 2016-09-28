2b94747fab1070074e0a771d938928c170dc60e8
Simon Jahreiß <simon.jahreiss@qaware.de>
2016-09-28 10:39:33 +0200

    #12 - Reimplemented MarathonResourceFactoryTest using WireMock.

--- deployer-marathon/src/test/java/de/qaware/cloud/deployer/marathon/resource/MarathonResourceFactoryTest.java
+++ deployer-marathon/src/test/java/de/qaware/cloud/deployer/marathon/resource/MarathonResourceFactoryTest.java
@@ -15,52 +15,90 @@
  */
 package de.qaware.cloud.deployer.marathon.resource;
 
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import de.qaware.cloud.deployer.commons.config.cloud.EnvironmentConfig;
 import de.qaware.cloud.deployer.commons.config.resource.ContentType;
 import de.qaware.cloud.deployer.commons.config.util.FileUtil;
 import de.qaware.cloud.deployer.commons.error.EnvironmentConfigException;
 import de.qaware.cloud.deployer.commons.error.ResourceConfigException;
 import de.qaware.cloud.deployer.commons.error.ResourceException;
+import de.qaware.cloud.deployer.commons.strategy.Strategy;
 import de.qaware.cloud.deployer.marathon.config.resource.MarathonResourceConfig;
 import de.qaware.cloud.deployer.marathon.resource.app.AppResource;
 import de.qaware.cloud.deployer.marathon.resource.base.MarathonResource;
 import de.qaware.cloud.deployer.marathon.resource.group.GroupResource;
 import de.qaware.cloud.deployer.marathon.test.MarathonTestEnvironmentUtil;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
 
 import java.io.IOException;
 
-public class MarathonResourceFactoryTest extends TestCase {
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static de.qaware.cloud.deployer.commons.logging.CommonsMessageBundle.COMMONS_MESSAGE_BUNDLE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MarathonResourceFactoryTest {
 
     private EnvironmentConfig environmentConfig;
 
-    @Override
+    @ClassRule
+    public static WireMockClassRule wireMockRule = new WireMockClassRule(WireMockConfiguration.options().dynamicPort());
+
+    @Rule
+    public WireMockClassRule instanceRule = wireMockRule;
+
+    @Before
     public void setUp() throws ResourceConfigException, ResourceException, EnvironmentConfigException, IOException {
-        environmentConfig = MarathonTestEnvironmentUtil.createTestEnvironment().getEnvironmentConfig();
+        instanceRule.stubFor(get(urlEqualTo("/service/marathon/ping"))
+                .willReturn(aResponse().withStatus(200)));
+
+        environmentConfig = new EnvironmentConfig("test", "http://localhost:" + instanceRule.port(), Strategy.REPLACE);
     }
 
-    @Override
-    public void tearDown() throws Exception {
-        super.tearDown();
+    @Test
+    public void testFactoryCreationWithFailingPing() throws ResourceConfigException {
+        boolean exceptionThrown = false;
+
+        instanceRule.resetMappings();
+        instanceRule.stubFor(get(urlEqualTo("/service/marathon/ping"))
+                .willReturn(aResponse().withStatus(404)));
+
+        try {
+            new MarathonResourceFactory(environmentConfig);
+        } catch (ResourceException e) {
+            exceptionThrown = true;
+            assertEquals(COMMONS_MESSAGE_BUNDLE.getMessage("DEPLOYER_COMMONS_ERROR_PING_FAILED", "test", 404), e.getMessage());
+        }
+        assertTrue(exceptionThrown);
     }
 
+    @Test
     public void testCreateResourceWithValidApp() throws ResourceException, ResourceConfigException {
-        MarathonResourceConfig config = new MarathonResourceConfig("", ContentType.JSON, FileUtil.readFileContent("/config/resource/app.json"));
+        MarathonResourceConfig config = new MarathonResourceConfig("test", ContentType.JSON, FileUtil.readFileContent("/de/qaware/cloud/deployer/marathon/resource/factory/app.json"));
         MarathonResourceFactory resourceFactory = new MarathonResourceFactory(environmentConfig);
         MarathonResource resource = resourceFactory.createResource(config);
         assertTrue(resource instanceof AppResource);
     }
 
+    @Test
     public void testCreateResourceWithValidGroup() throws ResourceException, ResourceConfigException {
-        MarathonResourceConfig config = new MarathonResourceConfig("", ContentType.JSON, FileUtil.readFileContent("/config/resource/group.json"));
+        MarathonResourceConfig config = new MarathonResourceConfig("test", ContentType.JSON, FileUtil.readFileContent("/de/qaware/cloud/deployer/marathon/resource/factory/group.json"));
         MarathonResourceFactory resourceFactory = new MarathonResourceFactory(environmentConfig);
         MarathonResource resource = resourceFactory.createResource(config);
         assertTrue(resource instanceof GroupResource);
     }
 
+    @Test
     public void testCreateResourceWithUnknownType() throws ResourceException, ResourceConfigException {
         boolean exceptionThrown = false;
-        MarathonResourceConfig config = new MarathonResourceConfig("", ContentType.JSON, FileUtil.readFileContent("/config/resource/unknown.json"));
+        MarathonResourceConfig config = new MarathonResourceConfig("test", ContentType.JSON, FileUtil.readFileContent("/de/qaware/cloud/deployer/marathon/resource/factory/unknown.json"));
         MarathonResourceFactory resourceFactory = new MarathonResourceFactory(environmentConfig);
         try {
             resourceFactory.createResource(config);
